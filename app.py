402f865bbba01bf0a881b7bdda3a18d3b45bdede
Leonuraht <72349155+Damcombot@users.noreply.github.com>
2024-06-27 17:54:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="AI Project",page_icon=":joy:",layout="wide")
+
+#Header---
+with st.container():
+    st.subheader("Hi!,Welcome to our AI project :wave:")
+    st.title("""It uses open cv and NLP to provide output""")
+    st.write("[Learn more >](https://blinderoggy.wixsite.com/vadarly)")
+
+with st.container():
+    st.write("---")
