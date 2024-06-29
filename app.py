d286db00ad98d6b7939855efd67838fbf32a4214
Leonuraht <72349155+Damcombot@users.noreply.github.com>
2024-06-29 15:03:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,11 +33,5 @@ with st.container():
 with st.container():
     left_column,right_column = st.columns(2)
     with left_column:
-        st.subheader("It uses :blue-background[NLP] and :red-background[CV]")
-        st.write("It can predict the ***presence*** of user ")
-        st.write("""These are the _*members*_ of our AI project :
-        \n-This is our project
-        \n-line1
-        \n-line2
-        \n-line3""")
-        st.write("other things...")
+        st.subheader("It uses :blue-background[NLP] and :red-background[CV] .")
+        st.write("It can predict the ***presence*** of user .")
