d6df07ee01ba11e2281b380feb589a7fc0d1477c
Leonuraht <72349155+Damcombot@users.noreply.github.com>
2024-06-29 22:06:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import streamlit as st
 st.set_page_config(page_title="Vadarly",page_icon=":part_alternation_mark:",layout="wide")
 
 #image---
-
-st.image("C:\Users\Jmbli\OneDrive\Documents\GitHub\silver-memory\Images\hellopic.png")
+image_path="C:\Users\Jmbli\OneDrive\Documents\GitHub\silver-memory\Images\hellopic.png"
+st.image(image_path,use_column_width=True)
 
 #hidder--
 hide_streamlit_style = """
@@ -38,11 +38,13 @@ with st.container():
             with left_column:
                         st.write("It uses :blue-background[NLP] and :red-background[CV] ")
                         st.write("It can predict the ***presence*** of user ")
-#camera-roll-test--                       
-picture = st.camera_input("Take a picture")
+#camera-roll-test-- 
 
-if picture:
-    st.image(picture)
+#picture = st.camera_input("Take a picture")
+
+#if picture:
+    #st.image(picture)
+
 #code-box--
 
 
