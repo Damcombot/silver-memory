953cb516923a4027312df6760cfcf431cfc61e69
Leonuraht <72349155+Damcombot@users.noreply.github.com>
2024-06-29 23:02:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Vadarly",page_icon=":part_alternation_mark:",layout="wide")
 
-#image---
+#images store---
 image_path= "https://th.bing.com/th/id/OIP._dP2SPVcLwJ-r_uKSsbpkwHaEK?rs=1&pid=ImgDetMain"
 
 
@@ -49,8 +49,8 @@ with st.container():
 st.code("""def fun():
          print(5+6)""")
 
-#image2---
-col1 , col2 = st.columns([1, 2])
+#image1---
+col1 , col2 = st.columns([2, 1])
 with col2:
             st.image(image_path,caption="Just a normal image")
 with col1:
