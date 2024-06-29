8a1dfab336091b153f6ada1ff64d9012fb99e267
Leonuraht <72349155+Damcombot@users.noreply.github.com>
2024-06-29 14:57:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.markdown(hide_github_icon, unsafe_allow_html=True)
 with st.container():
             st.title(""" Welcome to _:blue[Vadarly]_ """)
             st.subheader("Vadarly is a _voice assistant_ ")
-            st.write(---)
+            st.write("---")
 with st.container():
     left_column,right_column = st.columns(2)
     with left_column:
