fbfc869634e98a8d0da9857b797524764ff5501f
Leonuraht <72349155+Damcombot@users.noreply.github.com>
2024-07-01 21:18:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from audio_recorder_streamlit import audio_recorder
 # Initialize speech recognition and text-to-speech engines
 st.set_page_config(page_title="Vadarly",page_icon=":part_alternation_mark:",layout="wide")
 a=audio_recorder()
-if audio_bytes:
+if a:
     st.audio(audio_bytes, format="audio/wav")
 # Function to recognize speech from audio file
 def recognize_speech_from_audio_file(file):
