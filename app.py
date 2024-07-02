bdb7c269239b993f6134e998677555579af1e27b
Leonuraht <72349155+Damcombot@users.noreply.github.com>
2024-07-02 20:45:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ with st.container():
 with st.container():
             left_column,right_column = st.columns(2)
             with left_column:
-                        typewriter("It uses :blue-background[NLP] and :red-background[CV] ",4)
-                        typewriter("It can predict the ***presence*** of user ",4)
+                        typewriter("It uses :blue-background[NLP] and :red-background[CV] ",7)
+                        typewriter("It can predict the ***presence*** of user ",7)
 #camera-roll-test-- 
 
 #picture = st.camera_input("Take a picture")
